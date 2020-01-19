84e36740f41bbfa8349acd2892998e397802ffed
Mateusz Tarnawa <mjtarnawa@gmail.com>
2020-01-19 20:30:05 +0100

    Clean temp, add intermediate directory

--- mangadex-dl.py
+++ mangadex-dl.py
@@ -4,6 +4,7 @@ import os
 import sys
 import re
 import json
+import shutil
 from zipfile import ZipFile
 
 A_VERSION = "0.1.6"
@@ -43,6 +44,8 @@ def dl(mangadex_id, raw_chapters, download_path):
 		print("No chapters available to download!")
 		exit(0)
 	chapter_count = 0
+	pattern = re.compile(r'([^\s\w]|_)+')
+	sanitized_title = pattern.sub('', title)
 	for chapter_id in chaps_to_dl:
 		r = scraper.get("https://mangadex.org/api/chapter/{}/".format(chapter_id[1]))
 		chapter = json.loads(r.text)
@@ -54,21 +57,15 @@ def dl(mangadex_id, raw_chapters, download_path):
 		hashcode = chapter["hash"]
 		for page in chapter["page_array"]:
 			images.append("{}{}/{}".format(server, hashcode, page))
-
-		# download images
-		dest_folder = ""
-		pattern = re.compile(r'([^\s\w]|_)+')
-		sanitized_title = pattern.sub('', title)
 		manga_archive_name = "{0} - Chapter {1}.cbz".format(sanitized_title, chapter_id[0])
 		page_num = 1
+		dest_folder = os.path.join("/tmp", "download", "{0}_c{1}".format(mangadex_id, chapter_id[0]))
+		if not os.path.exists(dest_folder):
+			os.makedirs(dest_folder)
 		for img in images:
 			_, extension = os.path.splitext(img)
 			filename = str(page_num).zfill(3) + extension
-			dest_folder = os.path.join("/tmp", "download", "{0}_c{1}".format(mangadex_id, chapter_id[0]))
-			if not os.path.exists(dest_folder):
-				os.makedirs(dest_folder)
 			outfile = os.path.join(dest_folder, filename)
-
 			r = scraper.get(img)
 			if r.status_code == 200:
 				with open(outfile, 'wb') as f:
@@ -78,8 +75,9 @@ def dl(mangadex_id, raw_chapters, download_path):
 				sys.exit(1)
 			page_num += 1
 		files = get_all_file_paths(dest_folder)
-		create_cbz_archive(files, manga_archive_name, download_path)
+		create_cbz_archive(files, sanitized_title, manga_archive_name, download_path)
 		chapter_count += 1
+		shutil.rmtree(dest_folder)
 	print("Downloaded {0}/{1} chapters of: {2} into {3}".format(chapter_count, len(chaps_to_dl), title, download_path))
 	exit(0)
 
@@ -98,8 +96,8 @@ def gather_chapters(raw_chapters):
 	return requested_chapters
 
 
-def create_cbz_archive(file_paths, archive_name, base_path):
-	with ZipFile(os.path.join(base_path, archive_name), 'w') as z:
+def create_cbz_archive(file_paths, title, archive_name, base_path):
+	with ZipFile(os.path.join(base_path, title, archive_name), 'w') as z:
 		for file in file_paths:
 			z.write(file)
 
